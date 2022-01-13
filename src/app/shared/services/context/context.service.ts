19b4a78f2b4604602897f07fc15bc4c36105d832
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-01-13 18:28:15 +0300

    fixes

--- src/app/shared/services/context/context.service.ts
+++ src/app/shared/services/context/context.service.ts
@@ -1,27 +1,33 @@
-import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, defer, concat, EMPTY, ReplaySubject } from 'rxjs';
-import { switchMap, pluck, tap, mapTo, catchError, shareReplay, distinctUntilChanged } from 'rxjs/operators';
+import { Observable, defer, concat, EMPTY, ReplaySubject, of } from 'rxjs';
+import { switchMap, pluck, tap, mapTo, catchError, shareReplay, distinctUntilChanged, map } from 'rxjs/operators';
 
 import { OrganizationsService } from '@dsh/api';
 import { Organization } from '@dsh/api-codegen/organizations';
+
+const CONTEXT_ORGANIZATION_ID = 'context:organization_id';
 
 @Injectable()
 export class ContextService {
     organization$: Observable<Organization> = concat(
         defer(() => this.initOrganization$).pipe(
-            switchMap(() => this.organizationsService.getContext()),
-            pluck('organizationId'),
-            catchError((err) => {
-                if (err instanceof HttpErrorResponse && err.status === 404)
-                    return this.initOrganization().pipe(switchMap(() => EMPTY));
-                console.error(err);
-                return EMPTY;
-            })
+            // TODO: wait context service
+            // switchMap(() => this.organizationsService.getContext()),
+            // pluck('organizationId'),
+            // catchError((err) => {
+            //     if (err instanceof HttpErrorResponse && err.status === 404)
+            //         return this.initOrganization().pipe(switchMap(() => EMPTY));
+            //     console.error(err);
+            //     return EMPTY;
+            // })
+            map(() => window.localStorage.getItem(CONTEXT_ORGANIZATION_ID)),
+            switchMap((id) => (id ? of(id) : this.initOrganization().pipe(switchMap(() => EMPTY))))
         ),
         defer(() => this.switchOrganization$).pipe(
             distinctUntilChanged(),
-            switchMap((id) => this.organizationsService.switchContext(id).pipe(mapTo(id)))
+            // TODO: wait context service
+            // switchMap((id) => this.organizationsService.switchContext(id).pipe(mapTo(id)))
+            map((id) => (window.localStorage.setItem(CONTEXT_ORGANIZATION_ID, id), id))
         )
     ).pipe(
         switchMap((id) => this.organizationsService.getOrg(id)),
--- webpack.extra.js
+++ webpack.extra.js
@@ -1,5 +1,19 @@
 const webpack = require('webpack');
+const SentryWebpackPlugin = require('@sentry/webpack-plugin');
 
 module.exports = {
-    plugins: [new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)],
+    plugins: [
+        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+        ...(process.env.SENTRY_AUTH_TOKEN
+            ? [
+                  new SentryWebpackPlugin({
+                      authToken: process.env.SENTRY_AUTH_TOKEN,
+                      org: 'rbkmoney-fd',
+                      project: 'dashboard',
+                      include: './dist',
+                      ignore: ['node_modules', 'webpack.config.js'],
+                  }),
+              ]
+            : []),
+    ],
 };
