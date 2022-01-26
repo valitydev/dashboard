b56565cd191a28c54bdff6b273f582bebcaba168
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-01-26 15:25:30 +0300

    add retry

--- src/app/api/claims/claims.service.ts
+++ src/app/api/claims/claims.service.ts
@@ -1,6 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { first, switchMap } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { first, switchMap, catchError } from 'rxjs/operators';
 
 import {
     Claim,
@@ -97,7 +98,24 @@ export class ClaimsService {
         return this.keycloakTokenInfoService.partyID$.pipe(
             first(),
             switchMap((partyId) =>
-                this.claimsService.requestReviewClaimByID(this.idGenerator.shortUuid(), partyId, claimID, claimRevision)
+                this.claimsService
+                    .requestReviewClaimByID(this.idGenerator.shortUuid(), partyId, claimID, claimRevision)
+                    .pipe(
+                        catchError((err) => {
+                            if (err instanceof HttpErrorResponse && err.error?.code === 'invalidClaimRevision')
+                                return this.getClaimByID(claimID).pipe(
+                                    switchMap((claim) =>
+                                        this.claimsService.requestReviewClaimByID(
+                                            this.idGenerator.shortUuid(),
+                                            partyId,
+                                            claim.id,
+                                            claim.revision
+                                        )
+                                    )
+                                );
+                            return throwError(err);
+                        })
+                    )
             )
         );
     }
