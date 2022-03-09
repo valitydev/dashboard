a236f19a7de593bdfff44686f45a1c77918296f4
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-03-09 12:18:41 +0300

    add INR

--- src/app/shared/components/inputs/currency-autocomplete-field/currency-autocomplete-field.component.ts
+++ src/app/shared/components/inputs/currency-autocomplete-field/currency-autocomplete-field.component.ts
@@ -15,7 +15,7 @@ export class CurrencyAutocompleteFieldComponent extends WrappedFormControlSuperc
     @Input() label: string;
     @Input() @coerceBoolean required = false;
 
-    options: Option<string>[] = ['RUB', 'USD', 'EUR', 'UAH', 'KZT', 'BYN', 'JPY']
+    options: Option<string>[] = ['RUB', 'USD', 'EUR', 'UAH', 'KZT', 'BYN', 'JPY', 'INR']
         .sort()
         .map((currency) => ({ label: currency, value: currency }));
 
