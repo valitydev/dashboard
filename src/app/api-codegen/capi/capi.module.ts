d993fd3886d856018608e5c28cb63f427c25f534
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-04-05 01:44:07 +0300

    rem swag ShopsService

--- src/app/api-codegen/capi/capi.module.ts
+++ src/app/api-codegen/capi/capi.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 
 import { CapiConfigService } from './capi-config.service';
 import { InvoicesService } from './invoices.service';
-import { ShopsService } from './shops.service';
 import {
     ApiModule,
     ClaimsService,
@@ -19,14 +18,6 @@ import {
             providers: [{ provide: Configuration, useClass: CapiConfigService }],
         },
     ],
-    providers: [
-        CapiConfigService,
-        ClaimsService,
-        ShopsService,
-        InvoicesService,
-        PayoutsService,
-        PartiesService,
-        CountriesService,
-    ],
+    providers: [CapiConfigService, ClaimsService, InvoicesService, PayoutsService, PartiesService, CountriesService],
 })
 export class CapiModule {}
