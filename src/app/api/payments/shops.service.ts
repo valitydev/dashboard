91a97ee1f831a52e240c0d78812357ee860cf216
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-04-05 21:33:08 +0300

    add api wrapper

--- src/app/api/payments/shops.service.ts
+++ src/app/api/payments/shops.service.ts
@@ -3,15 +3,14 @@ import { ShopsService as ApiShopsService, Shop } from '@vality/swag-payments';
 import { defer, Observable, Subject } from 'rxjs';
 import { startWith, switchMapTo } from 'rxjs/operators';
 
-import { IdGeneratorService } from '@dsh/app/shared';
 import { shareReplayRefCount } from '@dsh/operators';
 
-import { createDefaultHeaders } from '../utils';
+import { createApi } from '../utils';
 
 @Injectable({
     providedIn: 'root',
 })
-export class ShopsService {
+export class ShopsService extends createApi(ApiShopsService) {
     shops$: Observable<Shop[]> = defer(() => this.reloadShops$).pipe(
         startWith(null),
         switchMapTo(this.getShops()),
@@ -20,27 +19,7 @@ export class ShopsService {
 
     private reloadShops$ = new Subject<void>();
 
-    constructor(private shopsService: ApiShopsService, private idGenerator: IdGeneratorService) {
-        this.shopsService.defaultHeaders = createDefaultHeaders();
-    }
-
-    getShopByID(shopID: string) {
-        return this.shopsService.getShopByID({ xRequestID: this.idGenerator.shortUuid(), shopID });
-    }
-
-    getShops() {
-        return this.shopsService.getShops({ xRequestID: this.idGenerator.shortUuid() });
-    }
-
     reloadShops(): void {
         this.reloadShops$.next();
-    }
-
-    suspendShop(shopID: string): Observable<void> {
-        return this.shopsService.suspendShop({ xRequestID: this.idGenerator.shortUuid(), shopID });
-    }
-
-    activateShop(shopID: string): Observable<void> {
-        return this.shopsService.activateShop({ xRequestID: this.idGenerator.shortUuid(), shopID });
     }
 }
