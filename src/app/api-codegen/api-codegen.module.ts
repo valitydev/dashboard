39f3d7a1f646be8ef31448c5d81456d61179b4ae
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-04-06 21:23:23 +0300

    rem capi module

--- src/app/api-codegen/api-codegen.module.ts
+++ src/app/api-codegen/api-codegen.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 
 import { AggrProxyModule } from './aggr-proxy';
 import { AnapiModule } from './anapi';
-import { CapiModule } from './capi';
 import { DarkApiModule } from './dark-api';
 import { MessagesModule } from './messages';
 import { OrganizationsModule } from './organizations';
 
 @NgModule({
-    imports: [CapiModule, AnapiModule, AggrProxyModule, DarkApiModule, MessagesModule, OrganizationsModule],
+    imports: [AnapiModule, AggrProxyModule, DarkApiModule, MessagesModule, OrganizationsModule],
 })
 /**
  * @deprecated there are intersections of services
--- src/app/api-codegen/capi/index.ts
+++ src/app/api-codegen/capi/index.ts
@@ -1,2 +1,1 @@
-export * from './capi.module';
 export * from './swagger-codegen';
