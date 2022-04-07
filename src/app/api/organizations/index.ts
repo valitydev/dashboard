fe590969c11d6a4b82869ad2a83759bec8d04f21
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-04-07 19:56:32 +0300

    rename

--- src/app/api/organizations/index.ts
+++ src/app/api/organizations/index.ts
@@ -1,5 +1,5 @@
 export * from './orgs.service';
-export * from './default-organization-name';
+export * from './utils';
 export * from './organizations.module';
 export * from './invitations.service';
 export * from './members.service';
