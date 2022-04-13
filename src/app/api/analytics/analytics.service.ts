5c2e9786738157ca1ed5d59c7443e732f1ff2788
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-04-13 16:22:26 +0300

    upd balances

--- src/app/api/analytics/analytics.service.ts
+++ src/app/api/analytics/analytics.service.ts
@@ -1,13 +1,10 @@
 import { Injectable } from '@angular/core';
-import { PaymentInstitution } from '@vality/swag-payments';
 import { Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 
-import { AnalyticsService as APIAnalyticsService, InlineResponse200, InlineResponse2001 } from '@dsh/api-codegen/anapi';
+import { AnalyticsService as APIAnalyticsService, InlineResponse200 } from '@dsh/api-codegen/anapi';
 import { KeycloakTokenInfoService } from '@dsh/app/shared/services';
 import { IdGeneratorService } from '@dsh/app/shared/services/id-generator';
-
-import RealmEnum = PaymentInstitution.RealmEnum;
 
 @Injectable()
 export class AnalyticsService {
@@ -18,25 +15,6 @@ export class AnalyticsService {
         private keycloakTokenInfoService: KeycloakTokenInfoService,
         private idGenerator: IdGeneratorService
     ) {}
-
-    getCurrentBalances(params: {
-        shopIDs?: string[];
-        excludeShopIDs?: string[];
-        paymentInstitutionRealm?: RealmEnum;
-    }): Observable<InlineResponse2001> {
-        return this.partyID$.pipe(
-            switchMap((partyID) =>
-                this.analyticsService.getCurrentBalances(
-                    this.idGenerator.shortUuid(),
-                    partyID,
-                    undefined,
-                    params.shopIDs,
-                    params.excludeShopIDs,
-                    params.paymentInstitutionRealm
-                )
-            )
-        );
-    }
 
     getGroupBalances(params: { shopIDs?: string[]; excludeShopIDs?: string[] }): Observable<InlineResponse200> {
         return this.partyID$.pipe(
