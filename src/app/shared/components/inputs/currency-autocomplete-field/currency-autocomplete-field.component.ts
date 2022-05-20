8430736414725f48f283a489c5e01362ebaed53f
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-05-20 15:43:18 +0300

    Add BRL currency(#67)

--- src/app/shared/components/inputs/currency-autocomplete-field/currency-autocomplete-field.component.ts
+++ src/app/shared/components/inputs/currency-autocomplete-field/currency-autocomplete-field.component.ts
@@ -14,7 +14,7 @@ export class CurrencyAutocompleteFieldComponent extends WrappedFormControlSuperc
     @Input() label: string;
     @Input() @coerceBoolean required = false;
 
-    options: Option<string>[] = ['RUB', 'USD', 'EUR', 'UAH', 'KZT', 'BYN', 'JPY', 'INR', 'AZN']
+    options: Option<string>[] = ['RUB', 'USD', 'EUR', 'UAH', 'KZT', 'BYN', 'JPY', 'INR', 'AZN', 'BRL']
         .sort()
         .map((currency) => ({ label: currency, value: currency }));
 
