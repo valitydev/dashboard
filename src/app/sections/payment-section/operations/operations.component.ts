c5cb90ec68ff08683e78dc6eb7478a0d5c52a999
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-07-29 15:04:24 +0300

    upd paymentSection/operations

--- src/app/sections/payment-section/operations/operations.component.ts
+++ src/app/sections/payment-section/operations/operations.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslocoService } from '@ngneat/transloco';
 
 @Component({
     templateUrl: 'operations.component.html',
@@ -7,12 +8,17 @@ export class OperationsComponent {
     links = [
         {
             path: 'payments',
+            label$: this.transloco.selectTranslate('operations.tabs.payments', null, 'payment-section'),
         },
         {
             path: 'invoices',
+            label$: this.transloco.selectTranslate('operations.tabs.invoices', null, 'payment-section'),
         },
         {
             path: 'refunds',
+            label$: this.transloco.selectTranslate('operations.tabs.refunds', null, 'payment-section'),
         },
     ];
+
+    constructor(private transloco: TranslocoService) {}
 }
