d1cbfd6561ca2e7e5a1186626cc51f7e63d836d2
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-07-29 15:14:21 +0300

    select errorCodes

--- src/app/api/anapi/anapi-dictionary.service.ts
+++ src/app/api/anapi/anapi-dictionary.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { TranslocoService } from '@ngneat/transloco';
-import { PaymentsToolDistributionResult } from '@vality/swag-anapi-v2';
 
 @Injectable({
     providedIn: 'root',
@@ -8,11 +7,49 @@ import { PaymentsToolDistributionResult } from '@vality/swag-anapi-v2';
 export class AnapiDictionaryService {
     constructor(private t: TranslocoService) {}
 
-    getPaymentToolLabels(): Record<PaymentsToolDistributionResult['name'], string> {
+    getPaymentToolLabels() {
         return {
-            bank_card: this.t.translate('analytics.paymentTool.bank_card', null, 'dictionary'),
-            digital_wallet: this.t.translate('analytics.paymentTool.digital_wallet', null, 'dictionary'),
-            payment_terminal: this.t.translate('analytics.paymentTool.payment_terminal', null, 'dictionary'),
+            bank_card: this.t.translate('analytics.paymentTools.bank_card', null, 'dictionary'),
+            digital_wallet: this.t.translate('analytics.paymentTools.digital_wallet', null, 'dictionary'),
+            payment_terminal: this.t.translate('analytics.paymentTools.payment_terminal', null, 'dictionary'),
+        };
+    }
+
+    getErrorCodeLabels() {
+        return {
+            account_limit_exceeded: this.t.translate('analytics.errorCodes.account_limit_exceeded', null, 'dictionary'),
+            account_not_found: this.t.translate('analytics.errorCodes.account_not_found', null, 'dictionary'),
+            amount: this.t.translate('analytics.errorCodes.amount', null, 'dictionary'),
+            authorization_failed: this.t.translate('analytics.errorCodes.authorization_failed', null, 'dictionary'),
+            bank_card_rejected: this.t.translate('analytics.errorCodes.bank_card_rejected', null, 'dictionary'),
+            card_expired: this.t.translate('analytics.errorCodes.card_expired', null, 'dictionary'),
+            card_number_invalid: this.t.translate('analytics.errorCodes.card_number_invalid', null, 'dictionary'),
+            insufficient_funds: this.t.translate('analytics.errorCodes.insufficient_funds', null, 'dictionary'),
+            no_route_found: this.t.translate('analytics.errorCodes.no_route_found', null, 'dictionary'),
+            number: this.t.translate('analytics.errorCodes.number', null, 'dictionary'),
+            operation_blocked: this.t.translate('analytics.errorCodes.operation_blocked', null, 'dictionary'),
+            operation_timeout: this.t.translate('analytics.errorCodes.operation_timeout', null, 'dictionary'),
+            payment_tool_rejected: this.t.translate('analytics.errorCodes.payment_tool_rejected', null, 'dictionary'),
+            preauthorization_failed: this.t.translate(
+                'analytics.errorCodes.preauthorization_failed',
+                null,
+                'dictionary'
+            ),
+            processing_deadline_reached: this.t.translate(
+                'analytics.errorCodes.processing_deadline_reached',
+                null,
+                'dictionary'
+            ),
+            rejected_by_issuer: this.t.translate('analytics.errorCodes.rejected_by_issuer', null, 'dictionary'),
+            risk_score_is_too_high: this.t.translate('analytics.errorCodes.risk_score_is_too_high', null, 'dictionary'),
+            security_policy_violated: this.t.translate(
+                'analytics.errorCodes.security_policy_violated',
+                null,
+                'dictionary'
+            ),
+            three_ds_failed: this.t.translate('analytics.errorCodes.three_ds_failed', null, 'dictionary'),
+            three_ds_not_finished: this.t.translate('analytics.errorCodes.three_ds_not_finished', null, 'dictionary'),
+            other: this.t.translate('analytics.errorCodes.other', null, 'dictionary'),
         };
     }
 }
--- src/app/sections/payment-section/analytics/payments-error-distribution/payments-error-distribution.service.ts
+++ src/app/sections/payment-section/analytics/payments-error-distribution/payments-error-distribution.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@angular/core';
-import { TranslocoService } from '@ngneat/transloco';
 import { BehaviorSubject, ReplaySubject, defer, combineLatest } from 'rxjs';
 import { map, pluck, switchMap } from 'rxjs/operators';
 
-import { AnalyticsService } from '@dsh/api/anapi';
+import { AnalyticsService, AnapiDictionaryService } from '@dsh/api/anapi';
+import { DictionaryService } from '@dsh/api/utils';
 import { shareReplayRefCount } from '@dsh/operators';
 import { errorTo, progressTo, distinctUntilChangedDeep, inProgressFrom, attach } from '@dsh/utils';
 
 import { SearchParams } from '../search-params';
 import { searchParamsToDistributionSearchParams } from '../utils';
-import { ErrorDistribution } from './error-distribution';
 import { errorsDistributionToChartData } from './errors-distribution-to-chart-data';
 import { getSelectedErrorDistribution } from './get-selected-error-distribution';
 import { subErrorsToErrorDistribution } from './sub-errors-to-error-distribution';
@@ -54,9 +53,15 @@ export class PaymentsErrorDistributionService {
     private selectedSubError$ = new BehaviorSubject<number[]>([]);
     private errorSub$ = new ReplaySubject<unknown>(1);
     private progress$ = new BehaviorSubject<number>(0);
-    private errorLabels$ = this.transloco.selectTranslation('payment-section').pipe(map(() => this.getErrorLabels()));
+    private errorLabels$ = this.dictionaryService.init$.pipe(
+        map(() => this.anapiDictionaryService.getErrorCodeLabels())
+    );
 
-    constructor(private analyticsService: AnalyticsService, private transloco: TranslocoService) {}
+    constructor(
+        private analyticsService: AnalyticsService,
+        private anapiDictionaryService: AnapiDictionaryService,
+        private dictionaryService: DictionaryService
+    ) {}
 
     updateSearchParams(searchParams: SearchParams) {
         this.searchParams$.next(searchParams);
@@ -68,91 +73,5 @@ export class PaymentsErrorDistributionService {
 
     goBackDataSelection() {
         this.selectedSubError$.next(this.selectedSubError$.getValue().slice(0, -1));
-    }
-
-    private getErrorLabels(): Record<ErrorDistribution['errorCode'], string> {
-        return {
-            account_limit_exceeded: this.transloco.translate(
-                'analytics.errorCodes.account_limit_exceeded',
-                null,
-                'payment-section'
-            ),
-            account_not_found: this.transloco.translate(
-                'analytics.errorCodes.account_not_found',
-                null,
-                'payment-section'
-            ),
-            amount: this.transloco.translate('analytics.errorCodes.amount', null, 'payment-section'),
-            authorization_failed: this.transloco.translate(
-                'analytics.errorCodes.authorization_failed',
-                null,
-                'payment-section'
-            ),
-            bank_card_rejected: this.transloco.translate(
-                'analytics.errorCodes.bank_card_rejected',
-                null,
-                'payment-section'
-            ),
-            card_expired: this.transloco.translate('analytics.errorCodes.card_expired', null, 'payment-section'),
-            card_number_invalid: this.transloco.translate(
-                'analytics.errorCodes.card_number_invalid',
-                null,
-                'payment-section'
-            ),
-            insufficient_funds: this.transloco.translate(
-                'analytics.errorCodes.insufficient_funds',
-                null,
-                'payment-section'
-            ),
-            no_route_found: this.transloco.translate('analytics.errorCodes.no_route_found', null, 'payment-section'),
-            number: this.transloco.translate('analytics.errorCodes.number', null, 'payment-section'),
-            operation_blocked: this.transloco.translate(
-                'analytics.errorCodes.operation_blocked',
-                null,
-                'payment-section'
-            ),
-            operation_timeout: this.transloco.translate(
-                'analytics.errorCodes.operation_timeout',
-                null,
-                'payment-section'
-            ),
-            payment_tool_rejected: this.transloco.translate(
-                'analytics.errorCodes.payment_tool_rejected',
-                null,
-                'payment-section'
-            ),
-            preauthorization_failed: this.transloco.translate(
-                'analytics.errorCodes.preauthorization_failed',
-                null,
-                'payment-section'
-            ),
-            processing_deadline_reached: this.transloco.translate(
-                'analytics.errorCodes.processing_deadline_reached',
-                null,
-                'payment-section'
-            ),
-            rejected_by_issuer: this.transloco.translate(
-                'analytics.errorCodes.rejected_by_issuer',
-                null,
-                'payment-section'
-            ),
-            risk_score_is_too_high: this.transloco.translate(
-                'analytics.errorCodes.risk_score_is_too_high',
-                null,
-                'payment-section'
-            ),
-            security_policy_violated: this.transloco.translate(
-                'analytics.errorCodes.security_policy_violated',
-                null,
-                'payment-section'
-            ),
-            three_ds_failed: this.transloco.translate('analytics.errorCodes.three_ds_failed', null, 'payment-section'),
-            three_ds_not_finished: this.transloco.translate(
-                'analytics.errorCodes.three_ds_not_finished',
-                null,
-                'payment-section'
-            ),
-            other: this.transloco.translate('analytics.errorCodes.other', null, 'payment-section'),
-        };
     }
 }
