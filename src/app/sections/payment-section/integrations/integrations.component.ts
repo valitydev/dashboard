0d065c533ab9a1636d233550a311ae6ee3b81f3f
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-07-29 14:55:10 +0300

    paymentSection.integrations.tabs

--- src/app/sections/payment-section/integrations/integrations.component.ts
+++ src/app/sections/payment-section/integrations/integrations.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslocoService } from '@ngneat/transloco';
 
 @Component({
     templateUrl: 'integrations.component.html',
@@ -7,12 +8,17 @@ export class IntegrationsComponent {
     links = [
         {
             path: 'payment-link',
+            label$: this.transloco.selectTranslate('integrations.tabs.payment-link', null, 'payment-section'),
         },
         {
             path: 'api-key',
+            label$: this.transloco.selectTranslate('integrations.tabs.api-key', null, 'payment-section'),
         },
         {
             path: 'webhooks',
+            label$: this.transloco.selectTranslate('integrations.tabs.webhooks', null, 'payment-section'),
         },
     ];
+
+    constructor(private transloco: TranslocoService) {}
 }
