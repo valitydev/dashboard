7fe93760f6cc3b32b80edc76f89165b682844a35
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-07-31 22:00:49 +0300

    daterange

---
+++ src/pipes/core-pipes-transloco-scope.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { TRANSLOCO_SCOPE } from '@ngneat/transloco';
+
+@NgModule({
+    providers: [{ provide: TRANSLOCO_SCOPE, useValue: 'core-pipes' }],
+})
+/**
+ * Used only to create a transloco-keys-manager scope file -
+ * no need to import anywhere (yet the library doesn't need it)
+ */
+export class CorePipesTranslocoScopeModule {}
