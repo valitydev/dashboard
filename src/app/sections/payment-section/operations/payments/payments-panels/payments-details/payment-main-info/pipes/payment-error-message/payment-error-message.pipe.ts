05a8e971baa5c5433acc420c8f69ff297d186ea1
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-08-01 23:35:25 +0300

    paymentErrorMessage

--- src/app/sections/payment-section/operations/payments/payments-panels/payments-details/payment-main-info/pipes/payment-error-message/payment-error-message.pipe.ts
+++ src/app/sections/payment-section/operations/payments/payments-panels/payments-details/payment-main-info/pipes/payment-error-message/payment-error-message.pipe.ts
@@ -8,7 +8,7 @@ import isObject from 'lodash-es/isObject';
     name: 'paymentErrorMessage',
 })
 export class PaymentErrorMessagePipe implements PipeTransform {
-    constructor(private transloco: TranslocoService) {}
+    constructor(private t: TranslocoService) {}
 
     transform(error: PaymentError): string {
         return this.formatErrors(error);
@@ -16,20 +16,24 @@ export class PaymentErrorMessagePipe implements PipeTransform {
 
     private formatErrors(error: PaymentError): string {
         let curError: PaymentError = error;
-        let translationPath = 'paymentErrors';
+        let translationPath = this.getErrorDict();
         let errorsMessage = '';
 
         while (isObject(curError)) {
             const { code, subError } = curError;
             let message: string;
-            translationPath = `${translationPath}.${code}`;
+            translationPath = translationPath?.[code];
 
             if (isObject(subError)) {
                 curError = subError;
-                message = this.translateError(`${translationPath}.message`);
+                const curMessage = translationPath?.['message'];
+                message = curMessage && typeof curMessage !== 'object' ? curMessage : this.t.translate('unknownError');
             } else {
                 curError = null;
-                message = this.translateError(translationPath);
+                message =
+                    translationPath && typeof translationPath !== 'object'
+                        ? translationPath
+                        : this.t.translate('unknownError');
             }
 
             errorsMessage = isEmpty(errorsMessage) ? message : `${errorsMessage} -> ${message}`;
@@ -38,10 +42,187 @@ export class PaymentErrorMessagePipe implements PipeTransform {
         return errorsMessage;
     }
 
-    private translateError(path: string): string {
-        const translation = this.transloco.translate(path);
-        return typeof translation === 'string' && translation !== path
-            ? translation
-            : this.transloco.translate('unknownError');
+    private getErrorDict() {
+        return {
+            authorization_failed: {
+                account_blocked: this.t.translate(
+                    'paymentErrorMessage.authorization_failed.account_blocked',
+                    null,
+                    'payment-section'
+                ),
+                account_limit_exceeded: {
+                    amount: this.t.translate(
+                        'paymentErrorMessage.authorization_failed.account_limit_exceeded.amount',
+                        null,
+                        'payment-section'
+                    ),
+                    message: this.t.translate(
+                        'paymentErrorMessage.authorization_failed.account_limit_exceeded.message',
+                        null,
+                        'payment-section'
+                    ),
+                    number: this.t.translate(
+                        'paymentErrorMessage.authorization_failed.account_limit_exceeded.number',
+                        null,
+                        'payment-section'
+                    ),
+                    unknown: this.t.translate(
+                        'paymentErrorMessage.authorization_failed.account_limit_exceeded.unknown',
+                        null,
+                        'payment-section'
+                    ),
+                },
+                account_not_found: this.t.translate(
+                    'paymentErrorMessage.authorization_failed.account_not_found',
+                    null,
+                    'payment-section'
+                ),
+                account_stolen: this.t.translate(
+                    'paymentErrorMessage.authorization_failed.account_stolen',
+                    null,
+                    'payment-section'
+                ),
+                insufficient_funds: this.t.translate(
+                    'paymentErrorMessage.authorization_failed.insufficient_funds',
+                    null,
+                    'payment-section'
+                ),
+                merchant_blocked: this.t.translate(
+                    'paymentErrorMessage.authorization_failed.merchant_blocked',
+                    null,
+                    'payment-section'
+                ),
+                message: this.t.translate('paymentErrorMessage.authorization_failed.message', null, 'payment-section'),
+                operation_blocked: this.t.translate(
+                    'paymentErrorMessage.authorization_failed.operation_blocked',
+                    null,
+                    'payment-section'
+                ),
+                payment_tool_rejected: {
+                    bank_card_rejected: {
+                        card_expired: this.t.translate(
+                            'paymentErrorMessage.authorization_failed.payment_tool_rejected.bank_card_rejected.card_expired',
+                            null,
+                            'payment-section'
+                        ),
+                        card_holder_invalid: this.t.translate(
+                            'paymentErrorMessage.authorization_failed.payment_tool_rejected.bank_card_rejected.card_holder_invalid',
+                            null,
+                            'payment-section'
+                        ),
+                        card_number_invalid: this.t.translate(
+                            'paymentErrorMessage.authorization_failed.payment_tool_rejected.bank_card_rejected.card_number_invalid',
+                            null,
+                            'payment-section'
+                        ),
+                        cvv_invalid: this.t.translate(
+                            'paymentErrorMessage.authorization_failed.payment_tool_rejected.bank_card_rejected.cvv_invalid',
+                            null,
+                            'payment-section'
+                        ),
+                        issuer_not_found: this.t.translate(
+                            'paymentErrorMessage.authorization_failed.payment_tool_rejected.bank_card_rejected.issuer_not_found',
+                            null,
+                            'payment-section'
+                        ),
+                        message: this.t.translate(
+                            'paymentErrorMessage.authorization_failed.payment_tool_rejected.bank_card_rejected.message',
+                            null,
+                            'payment-section'
+                        ),
+                        unknown: this.t.translate(
+                            'paymentErrorMessage.authorization_failed.payment_tool_rejected.bank_card_rejected.unknown',
+                            null,
+                            'payment-section'
+                        ),
+                    },
+                    message: this.t.translate(
+                        'paymentErrorMessage.authorization_failed.payment_tool_rejected.message',
+                        null,
+                        'payment-section'
+                    ),
+                    unknown: this.t.translate(
+                        'paymentErrorMessage.authorization_failed.payment_tool_rejected.unknown',
+                        null,
+                        'payment-section'
+                    ),
+                },
+                provider_limit_exceeded: {
+                    amount: this.t.translate(
+                        'paymentErrorMessage.authorization_failed.provider_limit_exceeded.amount',
+                        null,
+                        'payment-section'
+                    ),
+                    message: this.t.translate(
+                        'paymentErrorMessage.authorization_failed.provider_limit_exceeded.message',
+                        null,
+                        'payment-section'
+                    ),
+                    number: this.t.translate(
+                        'paymentErrorMessage.authorization_failed.provider_limit_exceeded.number',
+                        null,
+                        'payment-section'
+                    ),
+                    unknown: this.t.translate(
+                        'paymentErrorMessage.authorization_failed.provider_limit_exceeded.unknown',
+                        null,
+                        'payment-section'
+                    ),
+                },
+                rejected_by_issuer: this.t.translate(
+                    'paymentErrorMessage.authorization_failed.rejected_by_issuer',
+                    null,
+                    'payment-section'
+                ),
+                security_policy_violated: this.t.translate(
+                    'paymentErrorMessage.authorization_failed.security_policy_violated',
+                    null,
+                    'payment-section'
+                ),
+                temporarily_unavailable: this.t.translate(
+                    'paymentErrorMessage.authorization_failed.temporarily_unavailable',
+                    null,
+                    'payment-section'
+                ),
+                unknown: this.t.translate('paymentErrorMessage.authorization_failed.unknown', null, 'payment-section'),
+            },
+            no_route_found: {
+                message: this.t.translate('paymentErrorMessage.no_route_found.message', null, 'payment-section'),
+                risk_score_is_too_high: this.t.translate(
+                    'paymentErrorMessage.no_route_found.risk_score_is_too_high',
+                    null,
+                    'payment-section'
+                ),
+                unknown: this.t.translate('paymentErrorMessage.no_route_found.unknown', null, 'payment-section'),
+            },
+            preauthorization_failed: {
+                message: this.t.translate(
+                    'paymentErrorMessage.preauthorization_failed.message',
+                    null,
+                    'payment-section'
+                ),
+                three_ds_failed: this.t.translate(
+                    'paymentErrorMessage.preauthorization_failed.three_ds_failed',
+                    null,
+                    'payment-section'
+                ),
+                three_ds_not_finished: this.t.translate(
+                    'paymentErrorMessage.preauthorization_failed.three_ds_not_finished',
+                    null,
+                    'payment-section'
+                ),
+                unknown: this.t.translate(
+                    'paymentErrorMessage.preauthorization_failed.unknown',
+                    null,
+                    'payment-section'
+                ),
+            },
+            rejected_by_inspector: this.t.translate(
+                'paymentErrorMessage.rejected_by_inspector',
+                null,
+                'payment-section'
+            ),
+            timeout: this.t.translate('paymentErrorMessage.timeout', null, 'payment-section'),
+        };
     }
 }
