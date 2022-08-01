8ba7e4035f7dea6a8ec6cf3f880a35473a64aa37
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-08-01 15:37:10 +0300

    webhook

---
+++ src/app/api/payments/payments-dictionary.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { TranslocoService } from '@ngneat/transloco';
+import { CustomersTopic } from '@vality/swag-payments/lib/model/customers-topic';
+import { InvoicesTopic } from '@vality/swag-payments/lib/model/invoices-topic';
+
+import { DictionaryService } from '../utils';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class PaymentsDictionaryService {
+    invoicesTopicEventType$ = this.dictionaryService.create<InvoicesTopic.EventTypesEnum>(() => ({
+        /* eslint-disable @typescript-eslint/naming-convention */
+        InvoiceCreated: this.t.translate('payments.invoicesTopicEventType.InvoiceCreated', null, 'dictionary'),
+        InvoicePaid: this.t.translate('payments.invoicesTopicEventType.InvoicePaid', null, 'dictionary'),
+        InvoiceCancelled: this.t.translate('payments.invoicesTopicEventType.InvoiceCancelled', null, 'dictionary'),
+        InvoiceFulfilled: this.t.translate('payments.invoicesTopicEventType.InvoiceFulfilled', null, 'dictionary'),
+        PaymentStarted: this.t.translate('payments.invoicesTopicEventType.PaymentStarted', null, 'dictionary'),
+        PaymentProcessed: this.t.translate('payments.invoicesTopicEventType.PaymentProcessed', null, 'dictionary'),
+        PaymentCaptured: this.t.translate('payments.invoicesTopicEventType.PaymentCaptured', null, 'dictionary'),
+        PaymentCancelled: this.t.translate('payments.invoicesTopicEventType.PaymentCancelled', null, 'dictionary'),
+        PaymentRefunded: this.t.translate('payments.invoicesTopicEventType.PaymentRefunded', null, 'dictionary'),
+        PaymentFailed: this.t.translate('payments.invoicesTopicEventType.PaymentFailed', null, 'dictionary'),
+        PaymentRefundCreated: this.t.translate(
+            'payments.invoicesTopicEventType.PaymentRefundCreated',
+            null,
+            'dictionary'
+        ),
+        PaymentRefundSucceeded: this.t.translate(
+            'payments.invoicesTopicEventType.PaymentRefundSucceeded',
+            null,
+            'dictionary'
+        ),
+        PaymentRefundFailed: this.t.translate(
+            'payments.invoicesTopicEventType.PaymentRefundFailed',
+            null,
+            'dictionary'
+        ),
+        /* eslint-enable @typescript-eslint/naming-convention */
+    }));
+
+    customersTopicEventType$ = this.dictionaryService.create<CustomersTopic.EventTypesEnum>(() => ({
+        /* eslint-disable @typescript-eslint/naming-convention */
+        CustomerCreated: this.t.translate('payments.customersTopicEventType.CustomerCreated', null, 'dictionary'),
+        CustomerDeleted: this.t.translate('payments.customersTopicEventType.CustomerDeleted', null, 'dictionary'),
+        CustomerReady: this.t.translate('payments.customersTopicEventType.CustomerReady', null, 'dictionary'),
+        CustomerBindingStarted: this.t.translate(
+            'payments.customersTopicEventType.CustomerBindingStarted',
+            null,
+            'dictionary'
+        ),
+        CustomerBindingSucceeded: this.t.translate(
+            'payments.customersTopicEventType.CustomerBindingSucceeded',
+            null,
+            'dictionary'
+        ),
+        CustomerBindingFailed: this.t.translate(
+            'payments.customersTopicEventType.CustomerBindingFailed',
+            null,
+            'dictionary'
+        ),
+        /* eslint-enable @typescript-eslint/naming-convention */
+    }));
+
+    constructor(private t: TranslocoService, private dictionaryService: DictionaryService) {}
+}
--- src/app/sections/payment-section/integrations/webhooks/webhook-list/webhook-list.component.ts
+++ src/app/sections/payment-section/integrations/webhooks/webhook-list/webhook-list.component.ts
@@ -26,9 +26,13 @@ export class WebhookListComponent implements OnInit, OnDestroy {
     ngOnInit() {
         this.deleteWebhookService.init();
         this.deleteWebhookService.webhookDeleted$.subscribe(() => {
-            this.snackBar.open(this.transloco.translate('actions.delete.success', null, 'webhook'), 'OK', {
-                duration: 2000,
-            });
+            this.snackBar.open(
+                this.transloco.translate('webhook.actions.delete.success', null, 'payment-section'),
+                'OK',
+                {
+                    duration: 2000,
+                }
+            );
             this.refreshData.emit();
         });
     }
