e3d5dacf6216f9c8ba331ccdd60f6e5d540f4787
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2023-02-01 16:12:34 +0600

    add party id

--- src/app/api/payments/invoice-templates.service.ts
+++ src/app/api/payments/invoice-templates.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
 import { InvoiceTemplatesService as ApiInvoiceTemplatesService } from '@vality/swag-payments';
+import { switchMap, first, map } from 'rxjs/operators';
+
+import { ContextService } from '@dsh/app/shared';
 
 import { createApi } from '../utils';
 
 @Injectable({
     providedIn: 'root',
 })
-export class InvoiceTemplatesService extends createApi(ApiInvoiceTemplatesService) {}
+export class InvoiceTemplatesService extends createApi(ApiInvoiceTemplatesService) {
+    constructor(injector: Injector, private contextService: ContextService) {
+        super(injector);
+        const createInvoiceTemplate = this.createInvoiceTemplate;
+        this.createInvoiceTemplate = (p) =>
+            this.getPartyId().pipe(
+                switchMap((partyID) =>
+                    createInvoiceTemplate({
+                        ...p,
+                        invoiceTemplateCreateParams: { ...p.invoiceTemplateCreateParams, partyID },
+                    })
+                )
+            );
+    }
+
+    private getPartyId() {
+        return this.contextService.organization$.pipe(
+            first(),
+            map(({ party }) => party)
+        );
+    }
+}
--- src/app/api/payments/invoices.service.ts
+++ src/app/api/payments/invoices.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
 import { InvoicesService as ApiInvoicesService } from '@vality/swag-payments';
+import { switchMap } from 'rxjs';
+import { first, map } from 'rxjs/operators';
+
+import { ContextService } from '@dsh/app/shared';
 
 import { createApi } from '../utils';
 
 @Injectable({
     providedIn: 'root',
 })
-export class InvoicesService extends createApi(ApiInvoicesService) {}
+export class InvoicesService extends createApi(ApiInvoicesService) {
+    constructor(injector: Injector, private contextService: ContextService) {
+        super(injector);
+        const createInvoice = this.createInvoice;
+        this.createInvoice = (p) => {
+            return this.getPartyId().pipe(
+                switchMap((partyID) => createInvoice({ ...p, invoiceParams: { ...p.invoiceParams, partyID } }))
+            );
+        };
+    }
+
+    private getPartyId() {
+        return this.contextService.organization$.pipe(
+            first(),
+            map(({ party }) => party)
+        );
+    }
+}
