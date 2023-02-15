c556ad45107b174e16d3620b40843bf6ffb0037d
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2023-02-15 18:43:48 +0600

    fix translations

--- src/app/bootstrap.service.ts
+++ src/app/bootstrap.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { TranslocoService } from '@ngneat/transloco';
+import { Observable, throwError, switchMap } from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators';
 
-import { ShopsDataService } from '@dsh/app/shared';
+import { ShopsDataService, ErrorService, CommonError } from '@dsh/app/shared';
 
 @Injectable()
 export class BootstrapService {
-    bootstrapped$: Observable<boolean> = this.shopsDataService.shops$.pipe(map(() => true));
+    bootstrapped$: Observable<boolean> = this.shopsDataService.shops$.pipe(
+        catchError((err) =>
+            this.transloco.selectTranslate<string>('app.errors.bootstrapAppFailed', null, 'components').pipe(
+                tap((msg) => this.errorService.error(new CommonError(msg))),
+                switchMap(() => throwError(err))
+            )
+        ),
 
-    constructor(private shopsDataService: ShopsDataService) {}
+        map(() => true)
+    );
+
+    constructor(
+        private shopsDataService: ShopsDataService,
+        private transloco: TranslocoService,
+        private errorService: ErrorService
+    ) {}
 }
