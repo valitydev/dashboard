2c1f8b7fffa5a86a0ebd4e69c020a4daafc931a1
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2023-04-04 21:20:22 +0400

    fix

--- src/app/sections/payment-section/analytics/refunds-amount/refunds-amount.service.ts
+++ src/app/sections/payment-section/analytics/refunds-amount/refunds-amount.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { forkJoin, ReplaySubject, BehaviorSubject, defer } from 'rxjs';
-import { map, switchMap, withLatestFrom } from 'rxjs/operators';
+import { forkJoin, ReplaySubject, BehaviorSubject, defer, combineLatest } from 'rxjs';
+import { map, switchMap, distinctUntilChanged } from 'rxjs/operators';
 
 import { AnalyticsService } from '@dsh/api/anapi';
 import { shareReplayRefCount } from '@dsh/operators';
@@ -11,31 +11,37 @@ import { amountResultToStatData, searchParamsToStatSearchParams } from '../utils
 
 @Injectable()
 export class RefundsAmountService {
-    refundsAmount$ = defer(() => this.initialSearchParams$).pipe(
-        distinctUntilChangedDeep(),
-        map(searchParamsToStatSearchParams),
-        switchMap(({ current, previous, realm }) =>
-            forkJoin([
-                this.analyticsService.getRefundsAmount({ ...current, paymentInstitutionRealm: realm }),
-                this.analyticsService.getRefundsAmount({ ...previous, paymentInstitutionRealm: realm }),
-            ]).pipe(errorTo(this.errorSub$), progressTo(this.progress$))
+    refundsAmount$ = combineLatest([
+        defer(() => this.searchParams$).pipe(
+            map(searchParamsToStatSearchParams),
+            distinctUntilChangedDeep(),
+            switchMap(({ current, previous, realm }) =>
+                forkJoin([
+                    this.analyticsService.getRefundsAmount({ ...current, paymentInstitutionRealm: realm }),
+                    this.analyticsService.getRefundsAmount({ ...previous, paymentInstitutionRealm: realm }),
+                ]).pipe(errorTo(this.errorSub$), progressTo(this.progress$))
+            ),
+            map((res) => res.map((r) => r.result)),
+            map(amountResultToStatData)
         ),
-        map((res) => res.map((r) => r.result)),
-        map(amountResultToStatData),
-        withLatestFrom(defer(() => this.initialSearchParams$)),
-        map(([result, { currency }]) => result.find((r) => r.currency === currency)),
+        defer(() => this.searchParams$).pipe(
+            map(({ currency }) => currency),
+            distinctUntilChanged()
+        ),
+    ]).pipe(
+        map(([result, currency]) => result.find((r) => r.currency === currency)),
         shareReplayRefCount()
     );
     isLoading$ = inProgressFrom(() => this.progress$, this.refundsAmount$);
     error$ = attach(() => this.errorSub$, this.refundsAmount$);
 
-    private initialSearchParams$ = new ReplaySubject<SearchParams>(1);
+    private searchParams$ = new ReplaySubject<SearchParams>(1);
     private errorSub$ = new ReplaySubject<unknown>(1);
     private progress$ = new BehaviorSubject<number>(0);
 
     constructor(private analyticsService: AnalyticsService) {}
 
     updateSearchParams(searchParams: SearchParams): void {
-        this.initialSearchParams$.next(searchParams);
+        this.searchParams$.next(searchParams);
     }
 }
