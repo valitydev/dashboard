91e3324484ccd998a26a8271a4137f206a78d137
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2023-04-04 16:45:07 +0400

    fix averagePayment$

--- src/app/sections/payment-section/analytics/average-payment/average-payment.service.ts
+++ src/app/sections/payment-section/analytics/average-payment/average-payment.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { forkJoin, defer, ReplaySubject, BehaviorSubject } from 'rxjs';
-import { map, switchMap, withLatestFrom } from 'rxjs/operators';
+import { forkJoin, defer, ReplaySubject, BehaviorSubject, combineLatest } from 'rxjs';
+import { map, switchMap, distinctUntilChanged } from 'rxjs/operators';
 
 import { AnalyticsService } from '@dsh/api/anapi';
 import { shareReplayRefCount } from '@dsh/operators';
@@ -11,32 +11,37 @@ import { amountResultToStatData, searchParamsToStatSearchParams } from '../utils
 
 @Injectable()
 export class AveragePaymentService {
-    averagePayment$ = defer(() => this.searchParams$).pipe(
-        map(searchParamsToStatSearchParams),
-        distinctUntilChangedDeep(),
-        switchMap(({ current, previous, realm }) =>
-            forkJoin([
-                this.analyticsService.getAveragePayment({
-                    fromTime: current.fromTime,
-                    toTime: current.toTime,
-                    shopIDs: current.shopIDs,
-                    paymentInstitutionRealm: realm,
-                }),
-                this.analyticsService.getAveragePayment({
-                    fromTime: previous.fromTime,
-                    toTime: previous.toTime,
-                    shopIDs: previous.shopIDs,
-                    paymentInstitutionRealm: realm,
-                }),
-            ]).pipe(errorTo(this.errorSub$), progressTo(this.progress$))
+    averagePayment$ = combineLatest([
+        defer(() => this.searchParams$).pipe(
+            map(searchParamsToStatSearchParams),
+            distinctUntilChangedDeep(),
+            switchMap(({ current, previous, realm }) =>
+                forkJoin([
+                    this.analyticsService.getAveragePayment({
+                        fromTime: current.fromTime,
+                        toTime: current.toTime,
+                        shopIDs: current.shopIDs,
+                        paymentInstitutionRealm: realm,
+                    }),
+                    this.analyticsService.getAveragePayment({
+                        fromTime: previous.fromTime,
+                        toTime: previous.toTime,
+                        shopIDs: previous.shopIDs,
+                        paymentInstitutionRealm: realm,
+                    }),
+                ]).pipe(errorTo(this.errorSub$), progressTo(this.progress$))
+            ),
+            map((res) => res.map((r) => r.result)),
+            map(amountResultToStatData)
         ),
-        map((res) => res.map((r) => r.result)),
-        map(amountResultToStatData),
-        withLatestFrom(defer(() => this.searchParams$)),
-        map(([result, { currency }]) => result.find((r) => r.currency === currency)),
+        defer(() => this.searchParams$).pipe(
+            map(({ currency }) => currency),
+            distinctUntilChanged()
+        ),
+    ]).pipe(
+        map(([result, currency]) => result.find((r) => r.currency === currency)),
         shareReplayRefCount()
     );
-
     isLoading$ = inProgressFrom(() => this.progress$, this.averagePayment$);
     error$ = attach(() => this.errorSub$, this.averagePayment$);
 
