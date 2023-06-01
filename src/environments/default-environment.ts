63950c580489bae12a70adfb3710f6a81abae37e
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2023-06-01 12:46:05 +0400

    Fix stage det (#123)

--- src/environments/default-environment.ts
+++ src/environments/default-environment.ts
@@ -3,7 +3,7 @@ import { Environment } from './types/environment';
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const DEFAULT_ENVIRONMENT: Environment = {
     production: false,
-    stage: window.location.host.split('.')[0] === 'stage',
+    stage: window.location.host.split('.')[1] === 'stage',
     appConfigPath: '/appConfig.json',
     authConfigPath: '/authConfig.json',
 };
