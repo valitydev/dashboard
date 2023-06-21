3bdf87ffdc69b7e26d93a5aea3f457b82bf17a3b
A77AY <11846445+A77AY@users.noreply.github.com>
2023-06-21 16:27:57 +0400

    new buttons

--- src/app/dadata/dadata.module.ts
+++ src/app/dadata/dadata.module.ts
@@ -2,8 +2,8 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatLegacyAutocompleteModule as MatAutocompleteModule } from '@angular/material/legacy-autocomplete';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
 
 import { FormControlsModule } from '@dsh/components/form-controls';
