74fd66a9449fc653a1bc5ab6928d9b702d3e0499
A77AY <11846445+A77AY@users.noreply.github.com>
2023-06-26 20:50:44 +0400

    fox import

--- src/app/sections/payment-section/operations/refunds/refunds-search-filters/additional-filters/components/dialog-filters/dialog-filters.component.ts
+++ src/app/sections/payment-section/operations/refunds/refunds-search-filters/additional-filters/components/dialog-filters/dialog-filters.component.ts
@@ -1,10 +1,10 @@
 import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormGroupByValue } from '@vality/ng-core';
 
 import { ValidatedControlSuperclass, createControlProviders } from '@dsh/utils';
 
-import { FormGroupByValue } from '../../../../../../../../../../../ng-libs/projects/ng-core/dist';
 import { AdditionalFilters } from '../../types';
 
 @Component({
