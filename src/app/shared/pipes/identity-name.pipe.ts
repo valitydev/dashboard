5603a886cf7db94db6ce2b68d599c04072ce8a92
A77AY <11846445+A77AY@users.noreply.github.com>
2023-07-26 17:45:01 +0400

    ref identity name

---
+++ src/app/shared/pipes/identity-name.pipe.ts
@@ -0,0 +1,25 @@
+import { AsyncPipe } from '@angular/common';
+import { Pipe, PipeTransform, ChangeDetectorRef } from '@angular/core';
+import { Identity } from '@vality/swag-wallet';
+import { map } from 'rxjs/operators';
+
+import { IdentitiesService } from '@dsh/app/api/wallet';
+
+@Pipe({
+    name: 'identityName',
+    standalone: true,
+    pure: false,
+})
+export class IdentityNamePipe implements PipeTransform {
+    async = new AsyncPipe(this.cdr);
+
+    constructor(private cdr: ChangeDetectorRef, private identitiesService: IdentitiesService) {}
+
+    transform(id: Identity['id']): Identity['name'] {
+        return this.async.transform(
+            this.identitiesService.identities$.pipe(
+                map((identities) => identities.find((identity) => identity.id === id)?.name || id)
+            )
+        );
+    }
+}
