fe6438f83c7bff6946aab90ff5a319a8965b2814
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2023-10-13 19:32:52 +0700

    Bump swag wallet api, add identities api party id (#155)

--- src/app/api/wallet/identities.service.ts
+++ src/app/api/wallet/identities.service.ts
@@ -3,6 +3,7 @@ import { IdentitiesService as ApiIdentitiesService, Identity } from '@vality/swa
 import { Subject, defer, switchMap } from 'rxjs';
 import { startWith, map } from 'rxjs/operators';
 
+import { PartyIdExtension } from '@dsh/app/api/utils/extensions';
 import { shareReplayRefCount } from '@dsh/app/custom-operators';
 
 import { createApi } from '../utils';
@@ -10,7 +11,7 @@ import { createApi } from '../utils';
 @Injectable({
     providedIn: 'root',
 })
-export class IdentitiesService extends createApi(ApiIdentitiesService) {
+export class IdentitiesService extends createApi(ApiIdentitiesService, [PartyIdExtension]) {
     identities$ = defer(() => this.reloadIdentities$).pipe(
         startWith<void>(undefined),
         switchMap(() => this.listIdentities()),
