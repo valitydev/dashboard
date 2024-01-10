c0b63f71425bcdd39a25980bccdaa92c5db701d6
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-01-10 22:42:55 +0700

    upd provideTransloco

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { APP_INITIALIZER, ErrorHandler, LOCALE_ID, NgModule } from '@angular/core';
+import { APP_INITIALIZER, ErrorHandler, LOCALE_ID, NgModule, isDevMode } from '@angular/core';
 import {
     DateAdapter,
     MAT_DATE_FORMATS,
@@ -17,12 +17,7 @@ import {
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Router } from '@angular/router';
-import {
-    TRANSLOCO_CONFIG,
-    TRANSLOCO_LOADER,
-    TranslocoModule,
-    translocoConfig,
-} from '@ngneat/transloco';
+import { TranslocoModule, provideTransloco } from '@ngneat/transloco';
 import * as Sentry from '@sentry/angular-ivy';
 import { FlexLayoutModule } from 'ng-flex-layout';
 
@@ -112,17 +107,16 @@ import { TranslocoHttpLoaderService } from './transloco-http-loader.service';
         },
         { provide: MAT_RIPPLE_GLOBAL_OPTIONS, useValue: { disabled: true } },
         { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } },
-        {
-            provide: TRANSLOCO_CONFIG,
-            useValue: translocoConfig({
+        provideTransloco({
+            config: {
                 availableLangs: ['en', 'ru'],
                 defaultLang: 'en',
                 fallbackLang: 'ru',
                 reRenderOnLangChange: true,
-                prodMode: environment.production,
-            }),
-        },
-        { provide: TRANSLOCO_LOADER, useClass: TranslocoHttpLoaderService },
+                prodMode: !isDevMode(),
+            },
+            loader: TranslocoHttpLoaderService,
+        }),
         { provide: ENV, useValue: environment },
         {
             provide: ErrorHandler,
