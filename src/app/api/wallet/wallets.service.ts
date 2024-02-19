111e525867c0328e6c51500a42b9909c36cb8526
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-02-20 00:27:46 +0700

    IMP-165: Fix navbar when changed org, fix when a user was removed from the currently selected org, add mattermost notifications (#174)

--- src/app/api/wallet/wallets.service.ts
+++ src/app/api/wallet/wallets.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
 import { WalletsService as ApiWalletsService, Wallet } from '@vality/swag-wallet';
-import { of, Observable } from 'rxjs';
-import { catchError, map, pluck, shareReplay } from 'rxjs/operators';
+import { of, Observable, switchMap } from 'rxjs';
+import { catchError, shareReplay, map } from 'rxjs/operators';
 
-import { SHARE_REPLAY_CONF } from '@dsh/app/custom-operators';
+import { ContextOrganizationService } from '@dsh/app/shared';
 
 import { createApi } from '../utils';
 import { PartyIdExtension } from '../utils/extensions';
@@ -12,16 +12,20 @@ import { PartyIdExtension } from '../utils/extensions';
     providedIn: 'root',
 })
 export class WalletsService extends createApi(ApiWalletsService, [PartyIdExtension]) {
-    wallets$: Observable<Wallet[]> = this.listWallets({ limit: 1000 }).pipe(
-        catchError(() => of({ result: [] })),
-        pluck('result'),
-        shareReplay(SHARE_REPLAY_CONF),
+    wallets$: Observable<Wallet[]> = this.contextOrganizationService.organization$.pipe(
+        switchMap(() =>
+            this.listWallets({ limit: 1000 }).pipe(
+                map((v) => v.result),
+                catchError(() => of([])),
+            ),
+        ),
+        shareReplay({ refCount: true, bufferSize: 1 }),
     );
 
-    hasWallets$ = this.listWallets({ limit: 1 }).pipe(
-        catchError(() => of({ result: [] })),
-        pluck('result', 'length'),
-        map((l) => l > 0),
-        shareReplay(SHARE_REPLAY_CONF),
-    );
+    constructor(
+        injector: Injector,
+        private contextOrganizationService: ContextOrganizationService,
+    ) {
+        super(injector);
+    }
 }
