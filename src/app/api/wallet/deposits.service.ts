41c96c5d1f6d38eaf498aa7147271fca8a1ccf8b
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-02-27 21:32:23 +0700

    add PartyIdExtension

--- src/app/api/wallet/deposits.service.ts
+++ src/app/api/wallet/deposits.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { DepositsService as ApiDepositsService } from '@vality/swag-wallet';
 
+import { PartyIdExtension } from '@dsh/app/api/utils/extensions';
+
 import { createApi } from '../utils';
 
 @Injectable({
     providedIn: 'root',
 })
-export class DepositsService extends createApi(ApiDepositsService) {}
+export class DepositsService extends createApi(ApiDepositsService, [PartyIdExtension]) {}
