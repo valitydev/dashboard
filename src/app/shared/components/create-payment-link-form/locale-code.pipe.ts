319c1822c6b131c7188ee5f1efde99a2f9668c3b
Ildar Galeev <KeinAsylum@gmail.com>
2024-07-01 18:10:28 +0300

    Fix  transloco-keys-manager key detection

--- src/app/shared/components/create-payment-link-form/locale-code.pipe.ts
+++ src/app/shared/components/create-payment-link-form/locale-code.pipe.ts
@@ -1,6 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { TranslocoService } from '@ngneat/transloco';
-import { Observable, map } from 'rxjs';
 
 @Pipe({
     name: 'localeCode',
@@ -8,13 +7,29 @@ import { Observable, map } from 'rxjs';
 export class LocaleCode implements PipeTransform {
     constructor(private t: TranslocoService) {}
 
-    transform(localeCode: string): Observable<string> {
-        return this.t
-            .selectTranslation('components')
-            .pipe(
-                map(
-                    (keys) => keys[`createPaymentLinkForm.localeCodes.${localeCode}`] || localeCode,
-                ),
-            );
+    transform(localeCode: string): string {
+        // Duplications for transloco-keys-manager key detection
+        switch (localeCode) {
+            case 'ru':
+                return this.t.translate('createPaymentLinkForm.localeCodes.ru', null, 'components');
+            case 'en':
+                return this.t.translate('createPaymentLinkForm.localeCodes.en', null, 'components');
+            case 'ar':
+                return this.t.translate('createPaymentLinkForm.localeCodes.ar', null, 'components');
+            case 'az':
+                return this.t.translate('createPaymentLinkForm.localeCodes.az', null, 'components');
+            case 'bn':
+                return this.t.translate('createPaymentLinkForm.localeCodes.bn', null, 'components');
+            case 'ja':
+                return this.t.translate('createPaymentLinkForm.localeCodes.ja', null, 'components');
+            case 'ko':
+                return this.t.translate('createPaymentLinkForm.localeCodes.ko', null, 'components');
+            case 'pt':
+                return this.t.translate('createPaymentLinkForm.localeCodes.pt', null, 'components');
+            case 'tr':
+                return this.t.translate('createPaymentLinkForm.localeCodes.tr', null, 'components');
+            default:
+                return localeCode;
+        }
     }
 }
