82414ee6af270d66e87bace580decad03b216b6f
Ildar Galeev <KeinAsylum@gmail.com>
2024-07-16 14:22:55 +0300

    Return payment flow hold to payment link

--- src/app/shared/components/create-payment-link-form/types/controls.ts
+++ src/app/shared/components/create-payment-link-form/types/controls.ts
@@ -1,6 +1,11 @@
+import { HoldExpiration } from '@dsh/app/shared/services/create-payment-link/types/hold-expiration';
+
 export type Controls = {
     name: string;
     description: string;
     email: string;
     redirectUrl: string;
+    locale: null;
+    paymentFlowHold: false;
+    onHoldExpiration: HoldExpiration.Cancel;
 };
