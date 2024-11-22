61f626a643314a36e264dfbb358c8b5215df698d
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-11-22 21:37:13 +0800

    Revert "Revert changes of "New builder" (#200)"
    This reverts commit 0bfabdaab541d24b06d952e690dd40b9c35933bf.

--- src/app/api/claim-management/utils/sort-units.ts
+++ src/app/api/claim-management/utils/sort-units.ts
@@ -1,5 +1,5 @@
 import { ModificationUnit } from '@vality/swag-claim-management';
-import * as moment from 'moment';
+import moment from 'moment';
 
 export const sortUnitsByCreatedAtAsc = (
     { createdAt: a }: ModificationUnit,
--- src/environments/default-environment.ts
+++ src/environments/default-environment.ts
@@ -4,6 +4,6 @@ import { Environment } from './types/environment';
 export const DEFAULT_ENVIRONMENT: Environment = {
     production: false,
     stage: window.location.host.split('.')[1] === 'stage',
-    appConfigPath: '/appConfig.json',
-    authConfigPath: '/authConfig.json',
+    appConfigPath: '/assets/appConfig.json',
+    authConfigPath: '/assets/authConfig.json',
 };
