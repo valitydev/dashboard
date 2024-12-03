e161ca5c5e87430fb6eee87e285fc3eea0ed8c8f
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-12-03 19:49:47 +0800

    upd claims

--- src/app/sections/page-not-found/page-not-found.module.ts
+++ src/app/sections/page-not-found/page-not-found.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 import { TranslocoModule } from '@jsverse/transloco';
 import { FlexModule } from 'ng-flex-layout';
-
-import { ButtonModule } from '@dsh/components/buttons';
 
 import { PageNotFoundRoutingModule } from './page-not-found-routing.module';
 import { PageNotFoundComponent } from './page-not-found.component';
 
 @NgModule({
     declarations: [PageNotFoundComponent],
-    imports: [RouterModule, PageNotFoundRoutingModule, TranslocoModule, FlexModule, ButtonModule],
+    imports: [
+        RouterModule,
+        PageNotFoundRoutingModule,
+        TranslocoModule,
+        FlexModule,
+        MatButtonModule,
+    ],
     exports: [PageNotFoundComponent],
 })
 export class PageNotFoundModule {}
