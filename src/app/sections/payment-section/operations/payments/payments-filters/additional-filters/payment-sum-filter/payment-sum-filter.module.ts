404c7aaa4b69c2d1b3a17bcd99cefe42124fc2ed
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-12-10 18:55:46 +0700

    amount to number

--- src/app/sections/payment-section/operations/payments/payments-filters/additional-filters/payment-sum-filter/payment-sum-filter.module.ts
+++ src/app/sections/payment-section/operations/payments/payments-filters/additional-filters/payment-sum-filter/payment-sum-filter.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { TranslocoModule } from '@jsverse/transloco';
 import { FlexLayoutModule } from 'ng-flex-layout';
 
@@ -17,6 +18,7 @@ import { PaymentSumFilterComponent } from './payment-sum-filter.component';
         FlexLayoutModule,
         TranslocoModule,
         MatFormFieldModule,
+        MatInputModule,
     ],
     declarations: [PaymentSumFilterComponent],
     exports: [PaymentSumFilterComponent],
