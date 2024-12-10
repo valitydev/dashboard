89eb13ee6ff21f42c554479f92a256af11cb4a71
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-12-10 20:03:52 +0700

    upd inputs

--- src/app/sections/wallet-section/deposits/deposits-filters/additional-filters/deposit-sum-filter/deposit-sum-filter.module.ts
+++ src/app/sections/wallet-section/deposits/deposits-filters/additional-filters/deposit-sum-filter/deposit-sum-filter.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { TranslocoModule } from '@jsverse/transloco';
 import { FlexLayoutModule } from 'ng-flex-layout';
 
@@ -17,6 +18,7 @@ import { DepositSumFilterComponent } from './deposit-sum-filter.component';
         FlexLayoutModule,
         TranslocoModule,
         MatFormFieldModule,
+        MatInputModule,
     ],
     declarations: [DepositSumFilterComponent],
     exports: [DepositSumFilterComponent],
