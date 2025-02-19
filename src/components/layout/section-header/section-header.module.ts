dd1a784ae112022d60599563e7063594ce93a102
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2025-02-20 01:53:50 +0900

    add headline content-loading

--- src/components/layout/section-header/section-header.module.ts
+++ src/components/layout/section-header/section-header.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from 'ng-flex-layout';
 
 import { SectionHeaderComponent } from './section-header.component';
+import { ContentLoadingComponent } from '@vality/matez';
 
 @NgModule({
-    imports: [FlexLayoutModule],
+    imports: [FlexLayoutModule, ContentLoadingComponent],
     declarations: [SectionHeaderComponent],
     exports: [SectionHeaderComponent],
 })
