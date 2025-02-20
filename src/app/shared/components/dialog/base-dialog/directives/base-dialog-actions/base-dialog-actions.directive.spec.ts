dd8cb5e6feab86aff5ed605f860b5844b2d14edb
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2025-02-20 19:33:15 +0900

    add base cfg

--- src/app/shared/components/dialog/base-dialog/directives/base-dialog-actions/base-dialog-actions.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BaseDialogActionsDirective } from './base-dialog-actions.directive';
-
-describe('BaseDialogActionsDirective', () => {
-    it('should create an instance', () => {
-        const directive = new BaseDialogActionsDirective();
-        expect(directive).toBeTruthy();
-    });
-});
