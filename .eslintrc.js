2ce1a40eb8f53eb8fe800b8b42cbe9b4c941a247
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2025-02-20 19:09:09 +0900

    move to nx

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    extends: '@vality/eslint-config-ng',
-    overrides: [
-        ...require('@vality/eslint-config-ng/configs').angular('dsh').overrides,
-        ...require('@vality/eslint-config-ng/configs').importOrder(['@dsh/**']).overrides,
-    ],
-};
