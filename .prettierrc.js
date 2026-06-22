48d9076e6a6592fca8a46b747df717ff54ac8d37
Ray <11846445+A77AY@users.noreply.github.com>
2026-06-22 14:46:15 +0400

    chore(prettier): update Prettier configuration and add organize attributes plugin

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,1 +1,34 @@
-module.exports = require('@vality/ng-configs').prettierConfig;
+module.exports = {
+    printWidth: 100,
+    singleQuote: true,
+    tabWidth: 4,
+    plugins: ['prettier-plugin-organize-attributes'],
+    attributeSort: 'ASC',
+    attributeGroups: [
+        '$ANGULAR_ELEMENT_REF',
+        '$ANGULAR_STRUCTURAL_DIRECTIVE',
+        '$ANGULAR_ANIMATION',
+        '$ANGULAR_ANIMATION_INPUT',
+        '$ANGULAR_TWO_WAY_BINDING',
+        '$ANGULAR_INPUT',
+        '$DEFAULT',
+        '$ANGULAR_OUTPUT',
+    ],
+    overrides: [
+        {
+            files: '*.html',
+            options: { parser: 'angular' },
+        },
+        {
+            files: ['.{vscode,github}/**'],
+            options: {
+                tabWidth: 2,
+            },
+        },
+        {
+            files: '*.svg',
+            options: { parser: 'html' },
+        },
+    ],
+    endOfLine: 'auto',
+};
